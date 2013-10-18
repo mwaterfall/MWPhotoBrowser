639c61d7dcb64d8ca6321b3d1011045f5bd8c363
Michael Waterfall <michael@d3i.com>
2013-10-18 09:41:24 +0100

    Removed importing common defines and macros using the pch.

--- MWPhotoBrowser/Classes/MWCaptionView.m
+++ MWPhotoBrowser/Classes/MWCaptionView.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
+#import "MWCommon.h"
 #import "MWCaptionView.h"
 #import "MWPhoto.h"
 
--- MWPhotoBrowser/Classes/MWPhotoBrowser.m
+++ MWPhotoBrowser/Classes/MWPhotoBrowser.m
@@ -7,6 +7,7 @@
 //
 
 #import <QuartzCore/QuartzCore.h>
+#import "MWCommon.h"
 #import "MWPhotoBrowser.h"
 #import "MWZoomingScrollView.h"
 #import "MBProgressHUD.h"
