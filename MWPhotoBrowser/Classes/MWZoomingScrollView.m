6864225afe8e3cd69cc685686bef7e04a24e4af5
Michael Waterfall <michael@d3i.com>
2013-12-16 14:44:50 +0000

    zoomPhotosToFill option doesn't zoom standard images on a 4in display as it requires zooming in too far.

--- MWPhotoBrowser/Classes/MWZoomingScrollView.m
+++ MWPhotoBrowser/Classes/MWZoomingScrollView.m
@@ -206,6 +206,26 @@
 
 #pragma mark - Setup
 
+- (CGFloat)initialZoomScaleWithMinScale {
+    CGFloat zoomScale = self.minimumZoomScale;
+    if (_photoImageView && _photoBrowser.zoomPhotosToFill) {
+        // Zoom image to fill if the aspect ratios are fairly similar
+        CGSize boundsSize = self.bounds.size;
+        CGSize imageSize = _photoImageView.image.size;
+        CGFloat boundsAR = boundsSize.width / boundsSize.height;
+        CGFloat imageAR = imageSize.width / imageSize.height;
+        CGFloat xScale = boundsSize.width / imageSize.width;    // the scale needed to perfectly fit the image width-wise
+        CGFloat yScale = boundsSize.height / imageSize.height;  // the scale needed to perfectly fit the image height-wise
+        // Zooms standard portrait images on a 3.5in screen but not on a 4in screen.
+        if (ABS(boundsAR - imageAR) < 0.17) {
+            zoomScale = MAX(xScale, yScale);
+            // Ensure we don't zoom in or out too far, just in case
+            zoomScale = MIN(MAX(self.minimumZoomScale, zoomScale), self.maximumZoomScale);
+        }
+    }
+    return zoomScale;
+}
+
 - (void)setMaxMinZoomScalesForCurrentBounds {
 	
 	// Reset
@@ -213,12 +233,15 @@
 	self.minimumZoomScale = 1;
 	self.zoomScale = 1;
 	
-	// Bail
+	// Bail if no image
 	if (_photoImageView.image == nil) return;
+    
+	// Reset position
+	_photoImageView.frame = CGRectMake(0, 0, _photoImageView.frame.size.width, _photoImageView.frame.size.height);
 	
 	// Sizes
     CGSize boundsSize = self.bounds.size;
-    CGSize imageSize = _photoImageView.frame.size;
+    CGSize imageSize = _photoImageView.image.size;
     
     // Calculate Min
     CGFloat xScale = boundsSize.width / imageSize.width;    // the scale needed to perfectly fit the image width-wise
@@ -236,33 +259,19 @@
 	if (xScale >= 1 && yScale >= 1) {
 		minScale = 1.0;
 	}
-
-    // Initial zoom
-    CGFloat zoomScale = minScale;
-    if (_photoBrowser.zoomPhotosToFill) {
-        // Zoom image to fill if the aspect ratios are fairly similar
-        CGFloat boundsAR = boundsSize.width / boundsSize.height;
-        CGFloat imageAR = imageSize.width / imageSize.height;
-        if (ABS(boundsAR - imageAR) < 0.3) {
-            zoomScale = MAX(xScale, yScale);
-            // Ensure we don't zoom in or out too far, just in case
-            zoomScale = MIN(MAX(minScale, zoomScale), maxScale);
-        }
-    }
 	
-	// Set
+	// Set min/max zoom
 	self.maximumZoomScale = maxScale;
 	self.minimumZoomScale = minScale;
-	self.zoomScale = zoomScale;
     
-	// Reset position
-	_photoImageView.frame = CGRectMake(0, 0, _photoImageView.frame.size.width, _photoImageView.frame.size.height);
+    // Initial zoom
+    self.zoomScale = [self initialZoomScaleWithMinScale];
     
     // If we're zooming to fill then centralise
-    if (zoomScale != minScale) {
+    if (self.zoomScale != minScale) {
         // Centralise
-        self.contentOffset = CGPointMake((imageSize.width * zoomScale - boundsSize.width) / 2.0,
-                                         (imageSize.height * zoomScale - boundsSize.height) / 2.0);
+        self.contentOffset = CGPointMake((imageSize.width * self.zoomScale - boundsSize.width) / 2.0,
+                                         (imageSize.height * self.zoomScale - boundsSize.height) / 2.0);
         // Disable scrolling initially until the first pinch to fix issues with swiping on an initally zoomed in photo
         self.scrollEnabled = NO;
     }
