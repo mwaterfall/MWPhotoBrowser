1ada73e8564cbea175884ca0e427e428d8b742cc
fjarcticfox <fjarcticfox@gmail.com>
2015-09-28 15:44:45 +0800

    update SDWebImageOptions
    Prevent from getting error status "Domain=NSURLErrorDomain Code=-1100 " while reconnect to Internet and re-download the image.

--- Pod/Classes/MWPhoto.m
+++ Pod/Classes/MWPhoto.m
@@ -191,7 +191,7 @@
     @try {
         SDWebImageManager *manager = [SDWebImageManager sharedManager];
         _webImageOperation = [manager downloadImageWithURL:url
-                                                   options:0
+                                                   options:SDWebImageRetryFailed
                                                   progress:^(NSInteger receivedSize, NSInteger expectedSize) {
                                                       if (expectedSize > 0) {
                                                           float progress = receivedSize / (float)expectedSize;
