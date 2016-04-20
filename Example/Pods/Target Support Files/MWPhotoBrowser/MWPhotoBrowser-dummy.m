00ddfbc1aed5ae19e3d8b9797ccf6e0f93095295
Michael Waterfall <michaelwaterfall@gmail.com>
2016-04-20 17:45:46 +0100

    Updated pods

---
+++ Example/Pods/Target Support Files/MWPhotoBrowser/MWPhotoBrowser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MWPhotoBrowser : NSObject
+@end
+@implementation PodsDummy_MWPhotoBrowser
+@end
---
+++ Example/Pods/Target Support Files/Specta/Specta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Specta : NSObject
+@end
+@implementation PodsDummy_Specta
+@end
