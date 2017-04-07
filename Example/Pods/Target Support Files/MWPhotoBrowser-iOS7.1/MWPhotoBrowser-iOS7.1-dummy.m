1d09ce421f43a93d1c8c7833297682a50f590d02
william <chenw@lestata.com>
2017-04-07 09:45:25 +0800

    Modify podspec dependency library version. SDWebImage from 3.7 to 4.0, MBProgressHUD 0.9 to 1.0, DACircularProgress 2.0 to 2.3.1.

---
+++ Example/Pods/Target Support Files/MWPhotoBrowser-iOS7.1/MWPhotoBrowser-iOS7.1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MWPhotoBrowser_iOS7_1 : NSObject
+@end
+@implementation PodsDummy_MWPhotoBrowser_iOS7_1
+@end
---
+++ Example/Pods/Target Support Files/SDWebImage-iOS7.1/SDWebImage-iOS7.1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SDWebImage_iOS7_1 : NSObject
+@end
+@implementation PodsDummy_SDWebImage_iOS7_1
+@end
