3f7577717f7108999e1ec4e9db2e4b6ab0932c84
looseyi <13615033587@126.com>
2017-05-24 15:41:42 +0800

    change touchevent to gesture

--- MWPhotoBrowser/Classes/MWTapDetectingImageView.m
+++ MWPhotoBrowser/Classes/MWTapDetectingImageView.m
@@ -36,48 +36,56 @@
 
 - (void)addGestureRecognizer
 {
+    UITapGestureRecognizer *doubleTapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleDoubleTap:)];
+    doubleTapGesture.numberOfTapsRequired = 2;
+    [self addGestureRecognizer:doubleTapGesture];
+
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)];
+    [tapGesture requireGestureRecognizerToFail:doubleTapGesture];
+    [self addGestureRecognizer:tapGesture];
+
     UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
     [self addGestureRecognizer:longPressGesture];
 }
 
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-	UITouch *touch = [touches anyObject];
-	NSUInteger tapCount = touch.tapCount;
-	switch (tapCount) {
-		case 1:
-			[self handleSingleTap:touch];
-			break;
-		case 2:
-			[self handleDoubleTap:touch];
-			break;
-		case 3:
-			[self handleTripleTap:touch];
-			break;
-		default:
-			break;
-	}
+//	UITouch *touch = [touches anyObject];
+//	NSUInteger tapCount = touch.tapCount;
+//	switch (tapCount) {
+//		case 1:
+//			[self handleSingleTap:touch];
+//			break;
+//		case 2:
+//			[self handleDoubleTap:touch];
+//			break;
+//		case 3:
+//			[self handleTripleTap:touch];
+//			break;
+//		default:
+//			break;
+//	}
 	[[self nextResponder] touchesEnded:touches withEvent:event];
 }
 
 - (void)handleSingleTap:(UITouch *)touch {
-	if ([_tapDelegate respondsToSelector:@selector(imageView:singleTapDetected:)])
-		[_tapDelegate imageView:self singleTapDetected:touch];
-}
-
-- (void)handleDoubleTap:(UITouch *)touch {
-	if ([_tapDelegate respondsToSelector:@selector(imageView:doubleTapDetected:)])
-		[_tapDelegate imageView:self doubleTapDetected:touch];
-}
-
-- (void)handleTripleTap:(UITouch *)touch {
-	if ([_tapDelegate respondsToSelector:@selector(imageView:tripleTapDetected:)])
-		[_tapDelegate imageView:self tripleTapDetected:touch];
+    if ([_tapDelegate respondsToSelector:@selector(imageView:singleTapDetected:)])
+        [_tapDelegate imageView:self singleTapDetected:touch];
 }
 
 - (void)handleLongPress:(UITouch *)touch {
     if ([_tapDelegate respondsToSelector:@selector(imageView:handleLongPressed:)])
         [_tapDelegate imageView:self handleLongPressed:touch];
+}
+
+- (void)handleDoubleTap:(UITouch *)touch {
+    if ([_tapDelegate respondsToSelector:@selector(imageView:doubleTapDetected:)])
+        [_tapDelegate imageView:self doubleTapDetected:touch];
+}
+
+- (void)handleTripleTap:(UITouch *)touch {
+    if ([_tapDelegate respondsToSelector:@selector(imageView:tripleTapDetected:)])
+        [_tapDelegate imageView:self tripleTapDetected:touch];
 }
 
 @end
